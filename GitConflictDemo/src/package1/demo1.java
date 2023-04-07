fdc4ceef371b8b4f98e6ff5b2d7c16eab2ff1fd5
Shobha8 <shobhaprakash2013@gmail.com>
2023-04-07 18:57:56 -0400

    changed in 10 line

--- GitConflictDemo/src/package1/demo1.java
+++ GitConflictDemo/src/package1/demo1.java
@@ -5,7 +5,9 @@ public class demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello");
-	//
-	}
+		System.out.println("Hello Welcome");
+	
+	///
+		}
 
 }
